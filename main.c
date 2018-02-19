ab5edd868d03d6a7f2a7de70d42e6a314f6c405a
xjv <gregoire2broglie@gmail.com>
2018-02-19 17:18:47 +0100

    Ajout du header de main.

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "main.h"
+
 int main(int argc, char *argv[]) {
-  unsigned int valeur = 2867;
-  int milliers = (valeur - valeur % 1000) / 1000;
-  int centaines = (valeur % 1000 - valeur % 100) / 100;
-  int dizaines = (valeur % 100 - valeur % 10) / 10;
-  int unites = valeur % 10;
-  printf("%d\n%d\n%d\n%d\n",
-         milliers,
-         centaines,
-         dizaines,
-         unites);
   return 0;
 }
