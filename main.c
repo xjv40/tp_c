29889d541941e29ec327f2f9a7e02743b537f2f4
xjv <gregoire2broglie@gmail.com>
2018-02-19 16:53:47 +0100

    TP1 exo 4

--- main.c
+++ main.c
@@ -2,34 +2,15 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int nb_valeurs = 0;
-  int i = 0;
-  int nb_positifs = 0;
-  int nb_negatifs = 0;
-  int nb_nulles = 0;
-  int val_saisie = 0;
-
-  printf("Nombre de valeurs : ");
-  scanf("%d", &nb_valeurs);
-
-  for (i = 0; i < nb_valeurs; i++) {
-    val_saisie = 0;
-    printf("Valeur saisie : ");
-    scanf("%d", &val_saisie);
-    if (val_saisie < 0) {
-      nb_negatifs++;
-    } else if (val_saisie > 0){
-      nb_positifs++;
-    } else {
-      nb_nulles++;
-    }
-  }
-
-  printf("\n\n\n");
-  printf("valeurs nulles : %d\nvaleurs négatives : %d\nvaleurs positives : %d\n",
-          nb_nulles,
-          nb_negatifs,
-          nb_positifs);
-
+  unsigned int valeur = 2867;
+  int milliers = (valeur - valeur % 1000) / 1000;
+  int centaines = (valeur % 1000 - valeur % 100) / 100;
+  int dizaines = (valeur % 100 - valeur % 10) / 10;
+  int unites = valeur % 10;
+  printf("%d\n%d\n%d\n%d\n",
+         milliers,
+         centaines,
+         dizaines,
+         unites);
   return 0;
 }
