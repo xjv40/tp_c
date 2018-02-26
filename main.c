2b6bc6614dc89c46c442ee69616f7f29290d0b08
Grégoire de Broglie <gregoire2broglie@gmail.com>
2018-02-26 16:40:03 +0100

    TP1 exo 6

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "header/main.h"
 
 int main(int argc, char *argv[]) {
-  char saisie[100];
-  
-  printf("Veuillez saisir la chaine de caractere :\n");
-  scanf("%s", &saisie);
+  srand(time(NULL));
+  int nb_hasard = rand()%1000;
+  int saisie;
+  int nb_coups = 0;
 
-  int i = 0;
-  
-  while (saisie[i] != '\0') {
-    i++;
+  printf("Veuillez saisir votre nombre :\n");
+  scanf("%d", &saisie);
+
+  while(saisie != nb_hasard) {
+    if (saisie > nb_hasard) {
+      printf("Trop grand\n");
+    }
+    else {
+      printf("Trop petit\n");
+    }
+    nb_coups++;
+    printf("Veuillez saisir votre nouvelle valeur :\n");
+    scanf("%d", &saisie);
   }
 
-  printf("%s\n", saisie);
-  printf("%d\n", i);
+  printf("Vous avez trouve !\n");
+  printf("C'etait : %d\n", nb_hasard);
+  printf("Votre nombre de coups : %d\n", nb_coups);
   return 0;
 }
