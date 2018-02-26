342b9f271abf0357c5c3bcbd4c8483959eb5854b
Grégoire de Broglie <gregoire2broglie@gmail.com>
2018-02-26 16:19:56 +0100

    TP1 exo 5

--- main.c
+++ main.c
@@ -4,5 +4,18 @@
 #include "header/main.h"
 
 int main(int argc, char *argv[]) {
+  char saisie[100];
+  
+  printf("Veuillez saisir la chaine de caractere :\n");
+  scanf("%s", &saisie);
+
+  int i = 0;
+  
+  while (saisie[i] != '\0') {
+    i++;
+  }
+
+  printf("%s\n", saisie);
+  printf("%d\n", i);
   return 0;
 }
