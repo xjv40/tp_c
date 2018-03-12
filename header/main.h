9ea0442e85fa6b0fe61b0224c8a4eee8cfe8f92c
Grégoire de Broglie <gregoire2broglie@gmail.com>
2018-03-12 15:15:23 +0100

    Ajout du contrôle d'un tableau avec des pointeurs.

--- header/main.h
+++ header/main.h
@@ -1,4 +1,3 @@
 #ifndef __MAIN_H
 #define __MAIN_H
-void echange(int* a,int* b);
 #endif  // __MAIN_H
--- main.c
+++ main.c
@@ -4,33 +4,13 @@
 #include "header/main.h"
 
 int main(int argc, char* argv[]) {
-  int a = 4;
-  int b = 5;
+  int T[5] = {0};
+  int *p = T;
 
-  switch(argc){
-    case 1:
-      break;
-    case 2:
-      printf("Deux paramètres requis.\n");
-      return 2;
-    case 3:
-      a = atoi(argv[1]);
-      b = atoi(argv[2]);
-      break;
-    default:
-      printf("Deux paramètres sont nécessaires. \n");
-      return -1;
+  int i;
+  for (i = 0; i < 5; i++) {
+    *(p+i) = i;
+    printf("%d\n", *(p+i));
   }
-
-  printf("%d, %d\n", a, b);
-  echange(&a, &b);
-  printf("%d, %d\n", a, b);
   return 0;
-}
-
-void echange(int* a,int* b) {
-  int temp;
-  temp = *a;
-  *a = *b;
-  *b = temp;
 }
